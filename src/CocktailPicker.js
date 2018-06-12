d1b332a2924116bbb0f65e73ab1c070b4f6a335d
Victor Perez <vjperez@mix.wvu.edu>
2018-06-11 23:56:37 -0400

    Refactored and added glass images

--- src/CocktailPicker.js
+++ src/CocktailPicker.js
@@ -1,48 +1,88 @@
 import React, { Component } from 'react';
 import CocktailFile from './cocktails.json';
 import './CocktailPicker.css';
+import martiniGlass from './assets/martini-glass.svg';
+import shotGlass from './assets/shot-glass.svg';
+import tumblerGlass from './assets/tumbler-glass.svg';
+var images = [martiniGlass, shotGlass, tumblerGlass];
 
-// TODO: turn this into a class and add state for selected
-function Cocktail(props) {
+function Drink(props) {
   return (
     <div className="picker-frame__listItem">
       {props.name}
     </div>
   )
 }
-
-// TODO: add side component that collaps categories based on this class' state
-function CocktailsByGlass(props) {
-  const elements = (props.glass.cocktails).map((x) =>
-    <Cocktail key={x.name} name={x.name} />
-  );  
-  return (
-    <div className={"picker-frame__list picker-frame__list--" + props.glass.catName}>{elements}</div>
+function DrinkList(props) {
+  const elements = (props.drinks).map((x) =>
+    <Drink key={x.name} name={x.name} />
+  );
+  return(
+    <div className="picker-frame__list" > { elements }</div>
   );
 }
-class ProcessList extends React.Component{
+class CategoryHeader extends React.Component {
   render() {
-    var glasses = [];
-    (CocktailFile.categories).forEach(category => {
-      glasses.push(<div key={category.catName} className={"picker-frame__categoryPlate picker-frame__categoryPlate--"+ category.catName} >{category.catName}</div>);
-      glasses.push(<CocktailsByGlass key={category.catName + "_list"} glass={category} />)
+    return (
+      <div className={"picker-frame__categoryPlate picker-frame__categoryPlate--" + this.props.categoryName}>
+        <img src={getGlassImage(this.props.categoryName)} alt="" />
+        {this.props.categoryName}
+      </div>
+
+    );
+  }
+
+}
+class GlassCategory extends React.Component {
+  render() {
+    return (
+      <div className="picker-frame__category">
+        <CategoryHeader categoryName={this.props.categoryName} />
+        <DrinkList drinks={this.props.drinks} categoryName={this.props.categoryName} />
+      </div>
+    );
+  };
+}
+
+class PickerFrame extends React.Component {
+  render() {
+    var renderedCategories = [];
+    (this.props.drinkData).forEach(category => {
+      renderedCategories.push(<GlassCategory key={category[0].glass} categoryName={category[0].glass} drinks={category} />);
     });
-    return(
-      
-      <div className="picker-frame__elements">{glasses}</div>
+    return (
+      <div className="picker-frame">
+        <div className="picker-frame__header">Cocktail Picker </div>
+        {renderedCategories}
+      </div>
+
     );
   }
 }
+
+function loadData() {
+  var processedData = [];
+  for (var i = 0; i < (CocktailFile.categories).length; i++) {
+    var drinks = [];
+    ((CocktailFile.categories[i]).cocktails).forEach(drink => {
+      drinks.push(drink);
+    });
+    processedData.push(drinks);
+  }
+  return processedData;
+}
+
+function getGlassImage(glassName) {
+  for (var i = 0; i < images.length; i++) {
+    if (images[i].indexOf(glassName.toLowerCase()) !== -1)
+      return images[i];
+  }
+  return -1;
+}
+
 class App extends Component {
   render() {
-    return (
-      <div className="picker-frame">
-        <div className="picker-frame__header">
-        Cocktail Picker
-        </div>
-        <ProcessList />
-      </div>
-    );
+    return <PickerFrame drinkData={loadData()} />;
   }
 }
 
